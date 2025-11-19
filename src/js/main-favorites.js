59830ede2186e7183da89b07e063c8d1070b8423
YULIYA_KOSTENKO <yuliashkodina1@gmail.com>
2025-11-19 15:17:34 +0100

    1_Header structure, without definition_19.11.2025

--- src/js/main-favorites.js
+++ src/js/main-favorites.js
@@ -2,5 +2,6 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 import '/css/pages/favorites.css';
+import './header.js';
 
 console.log('fav')
--- src/js/main-home.js
+++ src/js/main-home.js
@@ -2,5 +2,9 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 import '/css/pages/home.css';
+import './header.js';
 
 console.log(iziToast)
+
+
+
