02ff086ce79654eea420f76fe60e230d70e294cc
Andrii Tsylyuryk <andrii.tsylyuryk@pipedrive.com>
2025-11-20 10:29:35 +0200

    Restore modal exercise functionality after merge with main

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+export const API_PROPS = Object.freeze({
+  BASE_URL: 'https://your-energy.b.goit.study/api',
+  EXERCISE_ENDPOINT: '/exercises',
+  RATING_ENDPOINT: '/rating',
+  FILTERS_ENDPOINT: '/filters',
+  QUOTE_ENDPOINT: '/quote',
+  SUBSCR_EDPOINT: '/subscription',
+});
