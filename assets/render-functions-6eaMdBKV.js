6b44acaf3dfe9c8cc13066329886f914cace3abe
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2025-11-24 05:57:01 +0000

    Deploying to gh-pages from @ JSA-project-group-1/project-17112025@ef3d098f3371cab7c149febfa69915cfe334ce0e 🚀

--- assets/render-functions-6eaMdBKV.js
+++ assets/render-functions-6eaMdBKV.js
@@ -245,5 +245,5 @@ import{a as u,b as B,i as m}from"./vendor-D2ogNlHo.js";(function(){const t=docum
       </li>
   `,T=g+p+y;s.innerHTML=T}function re(e,t,s){const a=e.map(o=>`
           <li class="filters-list-item ${t===o?"filters-list-item-active":""}" data-option="${o}"><p>${o}</p></li>
-      `).join("");s.innerHTML=a}async function ce(){try{const e=await ee(),t=document.querySelector(".quote-api-text"),s=document.querySelector(".quote-api-author");t&&(t.textContent=e.quote||"No quote available"),s&&(s.textContent=e.author||"Unknown author")}catch(e){console.error("Error rendering quote:",e)}}export{ie as a,ae as b,se as c,re as d,ce as e,oe as f,c as i,ne as r};
-//# sourceMappingURL=render-functions-CO5eAXIy.js.map
+      `).join("");s.innerHTML=a}async function ce(){try{const e=await ee(),t=document.querySelector(".quote-api-text"),s=document.querySelector(".quote-api-author");t&&(t.textContent=e.quote||"No quote available"),s&&(s.textContent=e.author||"Unknown author")}catch(e){console.error("Error rendering quote:",e)}}export{ie as a,ae as b,se as c,re as d,ce as e,oe as f,c as i,S as o,ne as r};
+//# sourceMappingURL=render-functions-6eaMdBKV.js.map
--- favorites.js
+++ favorites.js
@@ -1,16 +1,16 @@
-import{e as x,a as u,i as p}from"./assets/render-functions-CO5eAXIy.js";import{i as v}from"./assets/vendor-D2ogNlHo.js";const h="favorite_workouts",k=375,M=document.documentElement.clientWidth<=k,L=1440,S=document.documentElement.clientWidth>=L;let e=[],n=1,i=1,d=M?8:S?1e3:10;const o=document.querySelector("ul.pagination-controls-list"),c=document.querySelector("ul.favorites-list");document.getElementById("favorites-blok");function m(){const t=localStorage.getItem(h);if(!t)return[];try{const a=JSON.parse(t);return Array.isArray(a)?a:[]}catch(a){return console.error("Failed to parse favorites from LS:",a),v.error({message:"Favorites data is corrupted. Clearing storage.",position:"topRight",maxWidth:600}),localStorage.removeItem(h),[]}}function T(t){localStorage.setItem(h,JSON.stringify(t))}function F(){c.innerHTML="",o.innerHTML="";try{e=m(),i=Math.max(1,Math.ceil(e.length/d));const t=(n-1)*d,a=n*d,s=e.slice(t,a);g(s,c),e.length>0&&u(i,n,o)}catch(t){v.error({icon:"",position:"topRight",message:t.message})}}function g(t,a){if(t.length>0){const s=t.map(({_id:r,name:l,target:f,bodyPart:y,burnedCalories:b})=>`
+import{e as k,a as g,i as p,o as M}from"./assets/render-functions-6eaMdBKV.js";import{i as v}from"./assets/vendor-D2ogNlHo.js";const h="favorite_workouts",L=375,S=document.documentElement.clientWidth<=L,T=1440,F=document.documentElement.clientWidth>=T;let t=[],s=1,o=1,d=S?8:F?1e3:10;const c=document.querySelector("ul.pagination-controls-list"),l=document.querySelector("ul.favorites-list");document.getElementById("favorites-blok");function m(){const a=localStorage.getItem(h);if(!a)return[];try{const e=JSON.parse(a);return Array.isArray(e)?e:[]}catch(e){return console.error("Failed to parse favorites from LS:",e),v.error({message:"Favorites data is corrupted. Clearing storage.",position:"topRight",maxWidth:600}),localStorage.removeItem(h),[]}}function I(a){localStorage.setItem(h,JSON.stringify(a))}function w(){l.innerHTML="",c.innerHTML="";try{t=m(),o=Math.max(1,Math.ceil(t.length/d));const a=(s-1)*d,e=s*d,n=t.slice(a,e);u(n,l),t.length>0&&g(o,s,c)}catch(a){v.error({icon:"",position:"topRight",message:a.message})}}function u(a,e){if(a.length>0){const r=a.map(({_id:i,name:f,target:y,bodyPart:b,burnedCalories:x})=>`
     <li class="exercises-item">
     <div class="header-card">
     <div class="header-left">
         <span class="type">WORKOUT</span>
-          <button type="button" class="favorite-remove-btn" data-id="${r}">
+          <button type="button" class="favorite-remove-btn" data-id="${i}">
             <svg class="modal-exercise-trash-icon favorite-exercise-trash-icon" width="18" height="18">
               <use href="${p}#icon-trash"></use></svg>
             </span>
           </button>
        </div>
        <div class="header-right">
-        <button class="start-btn" type="button" data-modal-exercise="open" data-exercise-id="${r}"> Start
+        <button class="start-btn" type="button" data-modal-exercise="open" data-exercise-id="${i}"> Start
         <svg class="icon-arrow-right" width="18" height="18">
         <use href="${p}#icon-arrow-1"></use>
         </svg>
@@ -18,29 +18,29 @@ import{e as x,a as u,i as p}from"./assets/render-functions-CO5eAXIy.js";import{i
         </div>
         </div>
     <div class="title">
-      <button type="button" class="exercise-rating-btn js-give-rating-btn" data-exercise-id="${r}">
+      <button type="button" class="exercise-rating-btn js-give-rating-btn" data-exercise-id="${i}">
       <svg class="icon" width="24" height="24">
       <use href="${p}#icon-run-man-2"></use>
       </svg>
       </button>
-      <span class="name-text-exercise">${l}</span>
+      <span class="name-text-exercise">${f}</span>
     </div>
     <div class="details">
         <ul class="exercise-details-list">
         <li class="calories">
         <span class="calories-name">Burned calories</span>
-        <span class="calories-value">${b} / 3 min</span>
+        <span class="calories-value">${x} / 3 min</span>
         </li>
         <li class="body-part">
         <span class="body-part-name">Body part:</span>
-        <span class="body-part-value">${y}</span>
+        <span class="body-part-value">${b}</span>
         </li>
         <li class="target">
         <span class="target-name">Target:</span>
-        <span class="target-value">${f}</span>
+        <span class="target-value">${y}</span>
           </li>
           </ul>
           </div>
           </li>
-          `).join("");a.innerHTML=s}else a.innerHTML="<div class='no-content-warning'><p>It appears that you haven't added any exercises to your favorites yet.To get started, you can add exercises that you like to your favorites for easier access in the future.</p></div>"}function w(t){if(t.target.closest('[data-modal-exercise="close"]')){const s=m(),r=JSON.stringify(e),l=JSON.stringify(s);if(r===l)return;e=s,c.innerHTML="",o.innerHTML="",i=Math.max(1,Math.ceil(e.length/d)),g(e,c),e.length>0&&u(i,n,o)}}function I(t){const a=t.target.closest("button[data-id]");if(a){const s=a.dataset.id;if(!s)return;const r=e.filter(l=>l._id!==s);if(r.length===e.length)return;e=r,T(e),c.innerHTML="",o.innerHTML="",i=Math.max(1,Math.ceil(e.length/d)),g(e,c),e.length>0&&u(i,n,o)}}function $(t){const a=t.target.closest("button[data-page]");if(a)try{switch(a.dataset.page){case"beg":n=1;break;case"prev":n--;break;case"next":n++;break;case"end":n=i;break;default:n=parseInt(a.dataset.page,10)}const s=(n-1)*d,r=n*d,l=e.slice(s,r);g(l,c),e.length>0&&u(i,n,o)}catch(s){v.error({icon:"",position:"topRight",message:s.message})}}function E(){x(),F(),c.addEventListener("click",I),o.addEventListener("click",$),document.addEventListener("click",w)}E();
+          `).join("");e.innerHTML=r}else e.innerHTML="<div class='no-content-warning'><p>It appears that you haven't added any exercises to your favorites yet.To get started, you can add exercises that you like to your favorites for easier access in the future.</p></div>";e.querySelectorAll(".js-give-rating-btn").forEach(r=>{r.addEventListener("click",()=>{const i=r.dataset.exerciseId;M(i,null)})})}function E(a){if(a.target.closest('[data-modal-exercise="close"]')){const n=m(),r=JSON.stringify(t),i=JSON.stringify(n);if(r===i)return;t=n,l.innerHTML="",c.innerHTML="",o=Math.max(1,Math.ceil(t.length/d)),u(t,l),t.length>0&&g(o,s,c)}}function $(a){const e=a.target.closest("button[data-id]");if(e){const n=e.dataset.id;if(!n)return;const r=t.filter(i=>i._id!==n);if(r.length===t.length)return;t=r,I(t),l.innerHTML="",c.innerHTML="",o=Math.max(1,Math.ceil(t.length/d)),u(t,l),t.length>0&&g(o,s,c)}}function B(a){const e=a.target.closest("button[data-page]");if(e)try{switch(e.dataset.page){case"beg":s=1;break;case"prev":s--;break;case"next":s++;break;case"end":s=o;break;default:s=parseInt(e.dataset.page,10)}const n=(s-1)*d,r=s*d,i=t.slice(n,r);u(i,l),t.length>0&&g(o,s,c)}catch(n){v.error({icon:"",position:"topRight",message:n.message})}}function H(){k(),w(),l.addEventListener("click",$),c.addEventListener("click",B),document.addEventListener("click",E)}H();
 //# sourceMappingURL=favorites.js.map
